8709b4f50fc3b62e406664229027744d57657502
0xmihirsahu <0xmihirsahu@gmail.com>
2025-02-18 00:08:33 +0530

    less rusty

--- src/main.rs
+++ src/main.rs
@@ -39,13 +39,79 @@ fn play_game(){
             .expect("Failed to read line.");
 
         let moves: Vec<&str> = curr_input.split(',').collect();
-        let moves: Vec<u8> = moves.iter().filter_map(|x| x.parse().ok()).collect();
+        let moves: Vec<usize> = moves.iter().filter_map(|x| x.parse().ok()).collect();
         
         game_board[moves[0]][moves[1]] = curr_player;
+
+        match check_winner(&game_board){
+            PLAYER_X => {
+                print!("Player {} wins!", PLAYER_X);
+                break;
+            },
+            PLAYER_O => {
+                print!("Player {} wins!", PLAYER_O);
+            }
+
+            _ => {}
+        }
 
     }
     
     
+}
+
+fn check_winner(board: &BOARD) -> char{
+    match board{
+        [['X','X','X'],
+        [' ',' ',' '],
+        [' ',' ',' ']] => PLAYER_X,
+
+        [[' ',' ',' '],
+        ['X','X','X'],
+        [' ',' ',' ']] => PLAYER_X,
+
+        [[' ',' ',' '],
+        [' ',' ',' '],
+        ['X','X','X']] => PLAYER_X,
+
+        [['X',' ',' '],
+        [' ','X',' '],
+        [' ',' ','X']] => PLAYER_X,
+
+        [[' ',' ','X'],
+        [' ','X',' '],
+        ['X',' ',' ']] => PLAYER_X,
+
+        [[' ','X',' '],
+        [' ','X',' '],
+        [' ','X',' ']] => PLAYER_X,
+
+        [['O','O','O'],
+        [' ',' ',' '],
+        [' ',' ',' ']] => PLAYER_O,
+
+        [[' ',' ',' '],
+        ['O','O','O'],
+        [' ',' ',' ']] => PLAYER_O,
+
+        [[' ',' ',' '],
+        [' ',' ',' '],
+        ['O','O','O']] => PLAYER_O,
+
+        [['O',' ',' '],
+        [' ','O',' '],
+        [' ',' ','O']] => PLAYER_O,
+
+        [[' ',' ','O'],
+        [' ','O',' '],
+        ['O',' ',' ']] => PLAYER_O,
+
+        [[' ','O',' '],
+        [' ','O',' '],
+        [' ','O',' ']] => PLAYER_O,
+
+        _ => ' '
+    }
 }
 fn main() {
     println!("Welcome to rustactoe");
